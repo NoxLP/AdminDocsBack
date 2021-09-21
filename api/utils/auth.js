e3f687334ca4ba1ee7166cdff34585499dec1818
Norber <nox_nox_@hotmail.com>
2021-09-21 18:01:30 +0100

    Utils auth and handleError.

---
+++ api/utils/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+const createError = require('http-errors')
+const { User } = require('../models/users.model')
+
+exports.checkToken = async (req, res, next) => {
+  if (!req.headers?.token) {
+    return next(new createError.BadRequest('Token required in request headers'))
+  }
+  try {
+    const jwtToken = await jwt.verify(
+      req.headers?.token,
+      process.env.JWT_SECRET
+    )
+    res.locals.user = await User.find({
+      mobile_number: jwtToken.mobile_number,
+    }).exec()
+
+    if (!res.locals.user)
+      return next(new createError.BadRequest('user not valid'))
+
+    next()
+  } catch (err) {
+    next(err)
+  }
+}
+
+exports.createToken = (data) => {
+  return jwt.sign(data, process.env.SECRET, { expiresIn: '24h' })
+}
