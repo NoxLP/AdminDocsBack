1c31855ad5385c3d73e6f64d51497060e9ecc34a
Norber <nox_nox_@hotmail.com>
2021-09-22 08:08:53 +0100

    Fixed checkToken

--- api/utils/auth.js
+++ api/utils/auth.js
@@ -4,15 +4,15 @@ const { handleError } = require('./index')
 
 exports.checkToken = async (req, res, next) => {
   if (!req.headers?.token)
-    return handleError(400, 'Token required in request headers', res)
+    return handleError('Token required in request headers', res, 400)
 
   try {
     const jwtToken = await jwt.verify(req.headers?.token, process.env.SECRET)
-    res.locals.user = await UserModel.find({
+    res.locals.user = await UserModel.findOne({
       mobile_number: jwtToken.mobile_number,
     }).exec()
 
-    if (!res.locals.user) return handleError(400, 'user not valid', res)
+    if (!res.locals.user) return handleError('user not valid', res, 400)
 
     next()
   } catch (err) {
@@ -21,5 +21,9 @@ exports.checkToken = async (req, res, next) => {
 }
 
 exports.createToken = (user) => {
-  return jwt.sign({ id: user._id }, process.env.SECRET, { expiresIn: '24h' })
+  return jwt.sign(
+    { mobile_number: user.mobile_number, id: user._id },
+    process.env.SECRET,
+    { expiresIn: '24h' }
+  )
 }
